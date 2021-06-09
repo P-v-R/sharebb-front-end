4fcbcf8603cf1d3df5b414498c7f45411a4c476b
momes <mo.enokida@gmail.com>
2021-06-09 11:06:12 -0700

    merging

---
+++ src/api.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
+
+/** API Class.
+ *
+ * Static class tying together methods used to get/send to to the API.
+ * There shouldn't be any frontend-specific stuff here, and there shouldn't
+ * be any API-aware stuff elsewhere in the frontend.
+ *
+ */
+
+class ShareBnBApi {
+  // the token for interactive with the API will be stored here.
+  static token;
+
+  static async request(endpoint, data = {}, method = "get") {
+    console.debug("API Call:", endpoint, data, method);
+
+    const url = `${BASE_URL}/${endpoint}`;
+    const headers = { };
+    const params = (method === "get")
+      ? data
+      : {};
+
+    try {
+      return (await axios({ url, method, data, params, headers })).data;
+    } catch (err) {
+      console.error("API Error:", err.response);
+      let message = err.response.data.error.message;
+      throw Array.isArray(message) ? message : [message];
+    }
+  }
+
+  
+ // Individual API routes  
+ static async postImage(file) {
+      let res = await this.request(`image`, { file }, 'post');
+      return res;
+    }
+}
+export default ShareBnBApi;
