080de368773615b4e5dcd717ab6d6498411d2f33
Perryvon <Perry.j.vr@gmail.com>
2021-06-11 09:16:26 -0700

    more styling to homepage

--- src/Homepage.js
+++ src/Homepage.js
@@ -15,17 +15,20 @@ function Homepage({ currUser }) {
       <div className="row Homepage-background">
         <div className="row Homepage-background-opaque">
           <div className="col-10 offset-1">
-            <motion.div className="Homepage"
-            animate={{ x: 0 }}
-            initial={{ x: 2000 }}
-            transition={{delay:1}}
-            >
-              <h1 className="Logo">sharebnb</h1>
-              <p>share blah blah blah share</p>
-
-              <a href="/login"><button>Log In</button></a>
-              <a href="/signup"><button>Sign Up</button></a>
-            </motion.div>
+          <motion.div className="Homepage"
+          animate={{ x: 0 }}
+          initial={{ x: 2000 }}
+          transition={{delay:1}}
+          >
+            <h1 className="Logo">sharebnb</h1>
+          </motion.div>
+          <motion.div className="Homepage"
+          animate={{ x: 0 }}
+          initial={{ x: -2000 }}
+          transition={{delay:1.5}}
+          >
+            <p className="HomepageP">Where Sharing is Caring</p>
+          </motion.div>
           </div>
         </div>
       </div>
@@ -41,7 +44,13 @@ function Homepage({ currUser }) {
           transition={{delay:1}}
           >
             <h1 className="Logo">sharebnb</h1>
-            <p>share blah blah blah share</p>
+          </motion.div>
+          <motion.div className="Homepage"
+          animate={{ x: 0 }}
+          initial={{ x: -2000 }}
+          transition={{delay:1.5}}
+          >
+            <p className="HomepageP">Where Sharing is Caring</p>
           </motion.div>
         </div>
       </div>
